b941fc2c3f32395cdb39acbae33295f89bbe289d
Jannis Huebl <jannis.huebl@web.de>
2017-08-04 16:40:52 +0200

    add plugin system for add own generators

--- lib/orchparty/generators.rb
+++
@@ -1,7 +0,0 @@
-require 'orchparty/generators/docker_compose_v1'
-require 'orchparty/generators/docker_compose_v2'
-
-module Orchparty
-  module Generators
-  end
-end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,10 @@ SimpleCov.start
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "orchparty"
 
+
+Orchparty.plugin :docker_compose_v1
+Orchparty.plugin :docker_compose_v2
+
 def sort_ast(ast)
   Orchparty::Transformations::Sort.new.transform(ast)
 end
